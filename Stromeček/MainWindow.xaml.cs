b3e792f72b208bf50b3a09f96ff2879660536c5b
Aleš Janeček <Janecek.Ales@stud.vassboskovice.cz>
2021-12-21 09:36:41 +0100

    Přidejte soubory projektu.

---
+++ Stromeček/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stromeček
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer.Tick(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            int cislo = random.Next(0, 3);
+            if (cislo == 0)
+            { 
+                h.Source = (ImageSource) FindResource
+    }
+}
